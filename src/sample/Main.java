9a1ae6cc088abc0b30672089b081f9ab373540e2
SimaneAL <enamsiy9@gmail.com>
2021-12-19 14:16:23 +0100

    last commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception{
 		Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
 		primaryStage.setTitle("Shop S5 ");
-		primaryStage.setScene(new Scene(root, 900 , 700));
+		primaryStage.setScene(new Scene(root, 1100 , 700));
 		primaryStage.show();
 	}
 
